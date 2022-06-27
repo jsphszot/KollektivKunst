55bead76ace8f59e3f1520137a163e6bdf63099c
Joseph Szot <jsphszot@gmail.com>
2022-06-27 08:24:23 -0400

    blobby perlin noise - coding train learnin

---
+++ p5/cnv/blobby.js
@@ -0,0 +1,64 @@
+// https://www.youtube.com/watch?v=ZI1dmHv3MeM
+
+p5.disableFriendlyErrors = true; // disables FES
+
+let pi_splits = Math.PI*2/100;
+
+let noiseMax=5;
+let noise_inc=0.2;
+
+let min_r = 10;
+let max_r = 400;
+
+let phase=0;
+let phase_inc = 0.01;
+
+let offset_z = 0;
+let offset_z_inc = 0.01;
+
+
+
+function setup() {
+    canvasFitter();
+    // slider = createSlider(0, 10, 0, 0.1)
+}
+function windowResized() {
+    setup();
+    draw();
+}
+function draw() {
+        crazyCircle();
+}
+
+
+
+function crazyCircle(xpos=2, ypos=2) {
+    // noiseSeed(50);
+
+    // background(0);
+    // clear();
+    background([[0]*3, 10]);
+    translate(width/xpos, height/ypos);
+    stroke(255);
+    noFill();
+    beginShape();
+    // let noiseMax=slider.value();
+    // console.log(pi_splits);
+    for (let a=0; a<TWO_PI; a+=pi_splits) {
+        let offset_x = map(cos(a+phase), -1,1, 0,noiseMax);
+        let offset_y = map(sin(a), -1,1, 0,noiseMax);
+        let r = map(noise(offset_x,offset_y, offset_z), 0,1, min_r, max_r);
+        // let r = random(50, 100);
+        let x = r*cos(a);
+        let y = r*sin(a);
+        // vertex(x,y);
+        curveVertex(x,y);
+        // noiseMax += noise_inc;
+    }
+    // endShape();
+    endShape(CLOSE);
+    // noLoop();
+    phase += phase_inc;
+    offset_z += offset_z_inc;
+}
+
--- p5/cnv/grid.js
+++ p5/cnv/grid.js
@@ -5,52 +5,75 @@ var xpad;
 let yoffstart = 0;
 
 var rotx = 0;
-var divcatx;
-var divcaty;
 
 function setup() {
     canvasFitter();
     ypad = Math.min(height, width)*0.3; 
     xpad = ypad;
-    // frameRate(10);
+    frameRate(700);
+    rectMode(CENTER);
 }
 function windowResized() {
     setup();
     draw();
 }
 function draw() {
-    // noLoop()
-    
-    gridLines();
-    gridLines(sense=-1);
+    shapes();
 }
 
 
-function gridLines(sense=1) {
-    var xstart=xpad;
-    var xend=width-xpad;
-    var ystart=ypad;
-    var yend=height-ypad;
-    
-    
-    background(0);
-    // stroke(255);
-    
-    stroke(Math.random()*255, Math.random()*255, Math.random()*255);
-    // // [Math.random()*255,Math.random()*60,Math.random()*255, Math.random()*150]) // flor de jamaica
-    // stroke([Math.random()*255,Math.random()*60,Math.random()*255]); //, Math.random()*150]) // flor de jamaica);
-    strokeWeight(0.5);
+function cross_fill(i,j){
+    rect(i*100,j*100, 100,100);
+    rect(i*100,-j*100, 100,100);
+    rect(-i*100,-j*100, 100,100);
+    rect(-i*100,j*100, 100,100);
+}
+function board(i,j, sq_side){
+    // var canvas_area_squared = Math.floor(Math.sqrt(width*height));
+    // var sq_side = canvas_area_squared/100;
+    rect(i*sq_side,j*sq_side, sq_side,sq_side)
+    rect(-i*sq_side,j*sq_side, sq_side,sq_side)
+    rect(-i*sq_side,-j*sq_side, sq_side,sq_side)
+    rect(i*sq_side,-j*sq_side, sq_side,sq_side)
+}
 
-    // divcat=Math.random()*50;
-    divcatx=5;
-    divcaty=5;
-    for (let i=0; i<2000; i+=1){
-        translate(width/divcatx, height);
-        rotate(sense*rotx);
-        // line(xstart,ystart, xend, yend);
-        line(0,0, width, height);
+function circboard(i,j){
+    var canvas_area_squared = Math.floor(Math.sqrt(width*height));
+    var sq_side = canvas_area_squared/5;
+    // x,y, diameter
+    circle(i*sq_side,j*sq_side, sq_side)
+    circle(-i*sq_side,j*sq_side, sq_side)
+    circle(-i*sq_side,-j*sq_side, sq_side)
+    circle(i*sq_side,-j*sq_side, sq_side)
+}
+function shapes() {
+
+    background([[0]*3, 10]);
+    // stroke(255);
+    noStroke();
+    // stroke(255);
+    // strokeWeight(5);
+    //move the shape to the center of the canvas
+    // translate(mouseX, mouseY);
+    translate(width/2, height/2);
+    
+    var canvas_area_squared = Math.floor(Math.sqrt(width*height));
+    var sq_side = canvas_area_squared/5;
+    var ijmax = Math.max(width/sq_side, height/sq_side)*1.0;
+    // rotate((Math.random() > 0.97 ? 1 : -1)*rotx);
+    rotate(-rotx);
+    // rect(0,0, 100,100);
+    for (let i=1; i<ijmax; i+=1) {
+        for (let j=1; j<ijmax;j+=1){
+            let sq_color = 
+            fill([[255*(j%2)*(i%2)]*3, 200**(j%2)*(i%2)]);
+            // cross_fill(i,j);
+            board(i,j, sq_side);
+            // circboard(i,j);
+        }
     }
-    rotx += 0.008;
-    // console.log(rotx)
+    
+    rotx+=0.02;
+
 }
 
