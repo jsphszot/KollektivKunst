87641aca99bf9ce400ec795b6575904f66e7885a
Joseph Szot <jsphszot@gmail.com>
2022-06-28 12:26:31 -0400

    added control vars

--- p5/cnv/blobby.js
+++ p5/cnv/blobby.js
@@ -11,11 +11,13 @@ let min_r = 10;
 let max_r = 400;
 
 let phase=0;
-let phase_inc = 0.01;
+let phase_inc = 0.007;
 
 let offset_z = 0;
 let offset_z_inc = 0.01;
 
+// controls "fading" effect of drawing
+let bckg_alpha = 10;
 
 
 function setup() {
@@ -34,20 +36,21 @@ function draw() {
 
 function crazyCircle(xpos=2, ypos=2) {
     // noiseSeed(50);
-
     // background(0);
     // clear();
-    background([[0]*3, 10]);
+    background([[0]*3, bckg_alpha]);
     translate(width/xpos, height/ypos);
     stroke(255);
     noFill();
     beginShape();
     // let noiseMax=slider.value();
     // console.log(pi_splits);
+    // let max_r = Math.min(400, width);
+
     for (let a=0; a<TWO_PI; a+=pi_splits) {
         let offset_x = map(cos(a+phase), -1,1, 0,noiseMax);
         let offset_y = map(sin(a), -1,1, 0,noiseMax);
-        let r = map(noise(offset_x,offset_y, offset_z), 0,1, min_r, max_r);
+        let r = map(noise(offset_x,offset_y, offset_z), 0,1, min_r, Math.min(max_r, width*0.6));
         // let r = random(50, 100);
         let x = r*cos(a);
         let y = r*sin(a);
